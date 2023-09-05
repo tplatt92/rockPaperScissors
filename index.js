8aa38082db1bf8524f2dc00a7648ee09eddb6dfb
Tom Platt <thomasjplatt0107@gmail.com>
2023-09-05 20:43:10 +0100

    added computerPlay function

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const playerScore = 0;
 const computerScore = 0;
 const result = '';
 
+
+function computerPlay() {
+
 const choices = ['Rock', 'Paper', 'Scissors'];
+return choices[Math.floor(Math.random() * choices.length)];
 
-const randomChoice = () => Math.floor(Math.random()* choices.length);
+}
 
-console.log(randomChoice);
+computerPlay()
