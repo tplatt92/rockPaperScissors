b629a00302dcdd0777161124daec44e95d8e34ed
Tom Platt <thomasjplatt0107@gmail.com>
2023-09-06 08:25:27 +0100

    lowercase functions added

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 let playerScore = 0;
 let computerScore = 0;
 
-
-
 function getComputerChoice() {
-
-const choices = ['Rock', 'Paper', 'Scissors'];
-return choices[Math.floor(Math.random() * choices.length)];
-
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
-
-getComputerChoice()
-
-
 
 function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
-    let result = '';
+  let computerSelection = getComputerChoice().toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
+  let result = '';
 
-    if (playerSelection === computerSelection) {
-        result = `Tie`
-
-    } else if 
-    (playerSelection === 'Paper' && computerSelection === 'Rock'||
-    playerSelection === 'Rock' && computerSelection === 'Scissors'||
-    playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        
-        playerScore++;
-        result = 'Player Wins'
-
-    } else {
-        computerScore++;
-        result = `Computer Wins`;
-
-    }
-    return result; 
+  if (playerSelection === computerSelection) {
+    result = 'Tie';
+  } else if (
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    playerScore += 1;
+    result = 'Player Wins';
+  } else {
+    computerScore += 1;
+    result = 'Computer Wins';
+  }
+  return result;
 }
 
-playRound('Rock')
+console.log(playRound('Rock'));
+console.log(`Player Score: ${playerScore}`);
+console.log(`Computer Score: ${computerScore}`);
